d53168001ccd0b68da08b3ec859f0fc1303656df
Vu Anh Hao <hi@haova.me>
2023-04-04 10:37:41 +0700

    Replace webpack by vite

--- example/rugopa.config.js
+++ example/rugopa.config.js
@@ -1,17 +1,9 @@
 export default {
   sourceDir: './src',
-  assets: ['./assets/style.css', './assets/main.js', './images'],
-  statics: ['./favicon.ico'],
-  templates: ['./parts/header.ejs', './parts/footer.ejs', 'about.html'],
   routes: [
     {
       method: 'get',
       path: '/',
-      view: 'index.ejs',
-    },
-    {
-      method: 'get',
-      path: '/sample/:name.html',
       view: 'index.ejs',
     },
   ],
